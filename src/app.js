e6508500a2e212a893bd8e377a81aac0e1c6d54a
Ali Zahid Shahab <ali.zahid@konghamstageek.com>
2020-06-11 01:54:17 +0500

    Lesson 40 fully done

--- src/app.js
+++ src/app.js
@@ -50,15 +50,13 @@ class IndecisionApp extends React.Component {
   }
 }
 
-class Header extends React.Component {
-  render() {
+const Header = (props) => {
     return (
       <div>
-        <h1>{this.props.title}</h1>
-        <h2>{this.props.subtitle}</h2>
+        <h1>{props.title}</h1>
+        <h2>{props.subtitle}</h2>
       </div>
     );
-  }
 }
 
 const Action = (props) => {
